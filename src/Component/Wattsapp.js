b8c0453e47356b80a09d4d7fafb20c74d6ff9c0f
WorkSpaceTask2 <has328922@gmail.com>
2023-09-15 23:38:39 +0500

    portfolio commit

--- src/Component/Wattsapp.js
+++ src/Component/Wattsapp.js
@@ -4,7 +4,7 @@ const Wattsapp = () => {
     return (
         <>
             <div >
-                <a href="https:/wa.me/923184464379?text=Hi there, How can i help you !" target="_blank" className="whatsapp_float">
+                <a href="https://wa.me/923184464379?text=Hi there, How can i help you !" target="_blank" className="whatsapp_float">
                     <i className="fa-brands fa-whatsapp whatsapp-icon"></i>
                 </a>
             </div>
